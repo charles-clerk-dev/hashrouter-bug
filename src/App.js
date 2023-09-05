afc1ab182c26df01cc4025f38e9ebbf20cdd49ce
Charles Wefso <charles@clerk.dev>
2023-09-05 13:41:56 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ClerkProvider, SignedOut, useUser, SignIn } from "@clerk/clerk-react";
+import { HashRouter, Route } from "react-router-dom";
 
-function App() {
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw "Missing Publishable Key";
+}
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ClerkProvider clerkPubKey={clerkPubKey}>
+      <HashRouter>
+        <Route path="/" component={Welcome}>
+          <SignedOut>
+            <SignIn path="/login" routing="hash" redirectUrl="/" />
+          </SignedOut>
+        </Route>
+      </HashRouter>
+    </ClerkProvider>
   );
+};
+
+function Welcome() {
+  const user = useUser();
+  if (user) {
+    return <div>Hello {user.name}!</div>;
+  } else {
+    return <div>You are not signed in</div>;
+  }
 }
 
 export default App;
